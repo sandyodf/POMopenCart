40582cee715f71c3f8c075337f949cf14baa78ec
sandeep vadlapudi <vadlapudisanndeep@gmail.com>
2024-02-02 12:27:17 +0530

    using element in login,account page

--- src/main/java/com/qa/opencart/pages/AccountPage.java
+++ src/main/java/com/qa/opencart/pages/AccountPage.java
@@ -6,30 +6,37 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import com.qa.opencart.utils.ElementUtil;
+
 
 public class AccountPage {
 
 	private static WebDriver driver;
+	private ElementUtil elementUtil;
 	private By accountRightPaneLinks = By.cssSelector("div.list-group a");
 
 	public AccountPage(WebDriver driver) {
 		this.driver = driver;
+		this.elementUtil=new ElementUtil(driver);
 	}
 
 	public String getAccPageTitle() {
-		String title = driver.getTitle();
+//		String title = driver.getTitle();
+		String title = elementUtil.waitForTitleIsAndFetch(10,"My Account");
 
 		return title;
 	}
 
 	public String getAccpageUrl() {
-		String url = driver.getCurrentUrl();
+//		String url = driver.getCurrentUrl();
+		String url =elementUtil.waitForURLContainsAndFetch(10,"route=account/account");
 
 		return url;
 	}
 	
 	public List<WebElement> getaccountRightPaneLinks(){
-		return driver.findElements(accountRightPaneLinks);
+//		return driver.findElements(accountRightPaneLinks);
+		return elementUtil.waitForElementsVisible(accountRightPaneLinks, 10);
 	}
 	
 	public void clickOnAccountElements(String link) {
--- src/main/java/com/qa/opencart/pages/LoginPage.java
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -3,9 +3,12 @@ package com.qa.opencart.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import com.qa.opencart.utils.ElementUtil;
+
 public class LoginPage {
 
 	private WebDriver driver;
+	private ElementUtil elementUtil;
 	private By emailid = By.id("input-email");
 	private By passwordBy = By.id("input-password");
 	private By login_btn = By.xpath("//input[@value='Login']");
@@ -13,28 +16,34 @@ public class LoginPage {
 
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
+		this.elementUtil=new ElementUtil(driver);
 	}
 
 	public String getPageTitle() {
-		String title = driver.getTitle();
-
+//		String title = driver.getTitle();
+		String title = elementUtil.waitForTitleIsAndFetch(10,"Account Login");
 		return title;
 	}
 	
 	public String getpageUrl() {
-		String url = driver.getCurrentUrl();
-
+//		String url = driver.getCurrentUrl();
+		String url =elementUtil.waitForURLContainsAndFetch(10,"route=account/login");
 		return url;
 	}
 	
 	public boolean isForgotPassDisplayed() {
-		return driver.findElement(forgotPasswordlinkBy).isDisplayed();
+//		return driver.findElement(forgotPasswordlinkBy).isDisplayed();
+		return elementUtil.waitForElementVisible(forgotPasswordlinkBy,10).isDisplayed();
 	}
 	
 	public AccountPage dologiin(String un, String pwd) {
-		driver.findElement(emailid).sendKeys(un);
-		driver.findElement(passwordBy).sendKeys(pwd);
-		driver.findElement(login_btn).click();
+//		driver.findElement(emailid).sendKeys(un);
+//		driver.findElement(passwordBy).sendKeys(pwd);
+//		driver.findElement(login_btn).click();
+		
+		elementUtil.waitForElementVisible(emailid, 10);
+		elementUtil.doSendKeys(passwordBy, pwd);
+		elementUtil.doClick(login_btn);
         return new AccountPage(driver);
 		
 	}
